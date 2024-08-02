af13bba4f723077052b7c8d2ee52fff6954cc78a
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-08-02 15:52:48 -0500

    remove bad file.

--- scene/main/MainScene.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready():
-    var init_world = get_node("InitWorld")
-    var pc_move = get_node("PCMove")
-    init_world.connect("sprite_created", pc_move, "_on_InitWorld_sprite_created")
